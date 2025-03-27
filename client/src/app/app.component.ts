0b84d2959f56d3324a65f6054632dc4d5b535e6e
3ala2gh <3ala2mohammedgh@gmailcom>
2025-03-27 05:51:16 +0300

    completed login and registeration functionality

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,25 +2,39 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
+import { AccountService } from './_services/account.service';
+import { RegisterComponent } from "./register/register.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
   title = 'WellnessCore';
   http = inject(HttpClient);
+  private accountService = inject(AccountService);
   users: any;
+  registerMode = false;
 
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('complete')
-    })
+    this.setCurrentUser();
+  }
+
+  setCurrentUser()
+  {
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  cancelRegisterMode(event: boolean)
+  {
+    this.registerMode = event;
   }
 
 }
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  model: any ={}; 
+  accountService = inject(AccountService);
+  openRegister = output<boolean>();
 
+
+  login()
+  {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  logout()
+  {
+    this.accountService.logout();
+  }
+
+  openRegisterForm()
+  {
+    this.openRegister.emit(true);
+  }
 }
