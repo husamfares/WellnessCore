28ce75c530d43d3f9dec35c7876790a6d3596642
3ala2gh <3ala2mohammedgh@gmailcom>
2025-04-02 20:09:27 +0300

    Register Form check Validity

--- client/src/app/registerInfo/register/register.component.ts
+++ client/src/app/registerInfo/register/register.component.ts
@@ -1,21 +1,47 @@
-import { Component, inject, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, OnInit, output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../../_services/account.service';
 import { Router, RouterLink } from '@angular/router';
+import { JsonPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, ReactiveFormsModule,JsonPipe, NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+ 
   private router = inject(Router);
   model: any = {};
+  registerForm: FormGroup = new FormGroup({});
   private accountService = inject(AccountService);
   cancelRegister = output<boolean>();
   
 
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm()
+  {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('',[Validators.required, this.matchValues('password')])
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn
+  {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {isMatching: true}
+    }
+  }
 
   register()
   {
---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrL:'https://localhost:5001/api/',
+    hubsUrL:'https://localhost:5001/hubs/',
+};
