e30a4023fcf5f732662d9d21987696353bdf235e
3ala2gh <3ala2mohammedgh@gmailcom>
2025-04-20 18:50:18 +0300

    make the admin button visivle only for the admin

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -12,7 +12,7 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  private accountService = inject(AccountService);
+  accountService = inject(AccountService);
   private router = inject(Router);
   sidebarVisible = true;
 
