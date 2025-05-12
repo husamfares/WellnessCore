8dd62ab2e6589e56e0eea86c71c6393af2561490
3ala2gh <3ala2mohammedgh@gmailcom>
2025-05-12 22:54:54 +0300

    :)

--- API/Entities/ProfilePicture.cs
+++ API/Entities/ProfilePicture.cs
@@ -2,10 +2,7 @@ namespace API.Entities;
 
 public class ProfilePicture
 {
-<<<<<<< HEAD
-=======
    
->>>>>>> 564c8e85b8a961b04f49e00213a87a2014b9aafa
     public int Id { get; set; }
     public required string Url { get; set; }
     public int UserId { get; set; }
